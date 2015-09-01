b9a4a93f07d03baffe6c16e8a2f75c06a3953fb5
unknown <gewanbo@gmail.com>
2015-09-01 19:00:26 +0800

    init

---
+++ caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'port' => 3306,
+		'database' => 'db',
+		'username' => 'root',
+		'password' => '',
+		'tablepre' => 'p_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ phpsso_server/caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'miaoyu_bxg',
+		'username' => 'root',
+		'password' => '1234567890',
+		'tablepre' => 'my_sso_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		)
+);
+
+?>
